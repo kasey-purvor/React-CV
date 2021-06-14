028452b26f54929e91038215ba4f5f0e8d4f1258
kasey-purvor <kasey.purvor@gmail.com>
2021-06-14 13:47:10 +0100

    navbar styling changes

---
+++ src/components/Navbar.js
@@ -0,0 +1,33 @@
+import React, {useState} from 'react';
+import {Link} from 'react-router-dom';
+import './Navbar.css';
+
+function Navbar() {
+    return (
+        <>
+            <nav className="navbar">
+                <div className="nav-container">
+                    <ul className='item-container'>
+                        <li className='nav-item'>
+                            <Link>
+                                About Me
+                            </Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link>
+                                My Projects
+                            </Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link>
+                                Contact
+                            </Link>
+                        </li>
+                    </ul>
+
+                </div>
+            </nav>
+        </>
+    )
+}
+export default Navbar;
