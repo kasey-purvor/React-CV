3eea6f57e0765c1181d877c61658e72604fddfc7
kasey-purvor <kasey.purvor@gmail.com>
2021-06-15 10:50:12 +0100

    profile section styling edits, profile picture added

--- src/scenes/ProfilePage.js
+++ src/scenes/ProfilePage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './ProfilePage.css';
 
+
 class ProfilePage extends React.Component {
     constructor(props) {
         super(props)
@@ -10,33 +11,38 @@ class ProfilePage extends React.Component {
 
     render () {
         return (
-            <div className='profile-container'>
+            <div className='profile-page-container'>
                 <h1 className='profile-title'>
                     Kasey Purvor
                 </h1>
+
                 <h2 className='profile-role'>
                     Software Developer 
                 </h2>
-                <ul className='profile-details'>
+                <div className='image-details-container'>
+                    <img src='/images/dp2.png' className='dp' alt='Me' />
+                    <ul className='details'>
                     <span className='title'> About Me</span>
                     <li className='detail-item'>
-                         Name: Kasey Purvor    |    
+                         Name: Kasey Purvor  
                     </li>
                     <li className='detail-item'>
-                        Age: 26    |    
+                        Age: 26   
                     </li>
                     <li className='detail-item'>
                         Location: London
                     </li>
                 </ul>
+                </div>
+                
                 <p className='bio'>
                     <span className='title'> Hi I'm Kasey</span>
                     A mechanical engineer turned developer with a love of all things technical. I recently graduated from Makers Academy bootcamp and am now looking for my first junior web developer role. 
                     <br></br>
                     <br></br>
                     I like to own full projects and therefore want to perform full-stack development. With my past experience I have a diverse range of skills and knowledge including agile development, lean delivery, teamworking and much more. 
-                    
                 </p>
+                
             </div>
         )
     }
