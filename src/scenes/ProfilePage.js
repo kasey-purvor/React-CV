985477d3d6609829dee0aae04c36653e3b320401
kasey-purvor <kasey.purvor@gmail.com>
2021-06-15 13:13:51 +0100

    flex box introduced, more styuling changes. Attempts to use @media queries

--- src/scenes/ProfilePage.js
+++ src/scenes/ProfilePage.js
@@ -11,39 +11,48 @@ class ProfilePage extends React.Component {
 
     render () {
         return (
-            <div className='profile-page-container'>
-                <h1 className='profile-title'>
-                    Kasey Purvor
-                </h1>
+            <div className='page-container'>
+                <div className='profile-container'>
+                    <h1 className='section-title'>
+                        Kasey Purvor
+                    </h1>
 
-                <h2 className='profile-role'>
-                    Software Developer 
-                </h2>
-                <div className='image-details-container'>
-                    <img src='/images/dp2.png' className='dp' alt='Me' />
-                    <ul className='details'>
-                    <span className='title'> About Me</span>
-                    <li className='detail-item'>
-                         Name: Kasey Purvor  
-                    </li>
-                    <li className='detail-item'>
-                        Age: 26   
-                    </li>
-                    <li className='detail-item'>
-                        Location: London
-                    </li>
-                </ul>
+                    <h2 className='profile-role'>
+                        Software Developer 
+                    </h2>
+                    <div className='flex-container'>
+                        <div className='image-details-container'>
+                            <img src='/images/dp2.png' className='dp' alt='Me' />
+                            <ul className='details'>
+                                <span className='title'> About Me</span>
+                                <li className='detail-item'>
+                                 Name: Kasey Purvor  
+                                </li>
+                                <li className='detail-item'>
+                                  Age: 26   
+                                </li>
+                                <li className='detail-item'>
+                                    Location: London
+                                </li>
+                            </ul>
+                        </div>
+                        <p className='bio'>
+                            <span className='title'> Hi I'm Kasey</span>
+                            A mechanical engineer turned developer with a passion for technology. I recently graduated from Makers Academy bootcamp and am now looking for my first junior web developer role. 
+                            <br></br>
+                            <br></br>
+                            I like to own full projects and therefore want to perform full-stack development. With my past experience I have a diverse range of skills and knowledge including agile development, lean delivery, teamworking and much more. 
+                        </p>  
+                    </div>
                 </div>
-                
-                <p className='bio'>
-                    <span className='title'> Hi I'm Kasey</span>
-                    A mechanical engineer turned developer with a love of all things technical. I recently graduated from Makers Academy bootcamp and am now looking for my first junior web developer role. 
-                    <br></br>
-                    <br></br>
-                    I like to own full projects and therefore want to perform full-stack development. With my past experience I have a diverse range of skills and knowledge including agile development, lean delivery, teamworking and much more. 
-                </p>
-                
+                <div className='background-container'>
+                    <div className='work-experience-container'>
+                        <h1 className='section-title'>Work Experience</h1>
+                    </div>
+                </div>
             </div>
+
+           
         )
     }
 }
